fdc9456f36cdbdf07490c8c6106eb1dac9e9ded2
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-15 15:27:20 +0100

    CS

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,11 +1,20 @@
 <?php
+/**
+ * kiwi-suite/database (https://github.com/kiwi-suite/project-uri)
+ *
+ * @package kiwi-suite/project-uri
+ * @see https://github.com/kiwi-suite/project-uri
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
 namespace KiwiSuite\ProjectUri;
 
 use KiwiSuite\Contract\Application\ConfigProviderInterface;
 
 final class ConfigProvider implements ConfigProviderInterface
 {
-
     public function __invoke(): array
     {
         return [
